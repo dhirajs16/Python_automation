fd83875642f7c0aaf6e2c2a9f02cabb85c53ec70
dhirajs16 <sarrafd16@gmail.com>
2024-10-07 07:09:27 +0545

    Completed the login action template in Project01

--- Project01/main.py
+++ Project01/main.py
@@ -5,9 +5,11 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.keys import Keys
 import time
 
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
+
+# for waiting for the element to be clickable
+# from selenium.webdriver.support.ui import WebDriverWait
+# from selenium.webdriver.support import expected_conditions as EC
+# from selenium.webdriver.common.by import By
 
 def get_driver():
     options = webdriver.ChromeOptions()
@@ -27,16 +29,14 @@ def get_driver():
 
 def main():
     driver = get_driver()
-    # time.sleep(2)
-    driver.find_element(by='id', value='user_email').send_keys('sp4105043@gmail.com' + Keys.RETURN)
+    driver.find_element(by='id', value='user_email').send_keys('sp4105043@gmail.com' + Keys.ENTER)
     time.sleep(2)
-    button = driver.find_element(by='xpath', value='/html/body/div[1]/div[2]/div/div/div[3]/form/button')
-    action = ActionChains(driver)
-    action.double_click(button).perform()
-    
+    # driver.find_element(by='xpath', value='/html/body/div[1]/div[2]/div/div/div[3]/form/button').click()
     time.sleep(5)
 
-
+    # To chain actions
+    # action = ActionChains(driver)
+    # action.double_click(button).perform()
 
     # driver.execute_script("arguments[0].click();", button) 
     # WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/div[2]/div/div/div[3]/form/button'))).click()
